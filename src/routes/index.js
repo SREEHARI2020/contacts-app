a57a56d30483918b30cd4b3ca59c40c0caec2b86
SREEHARI <sreehari2610@gmail.com>
2021-01-13 17:15:31 +0530

    initial

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+const routes=[
+    {
+        path:"/auth/register",
+        component:RegisterComponent
+    },
+    {
+        path:"/auth/login",
+        component:LoginComponent
+    },
+    {
+        path:"/",
+        component:ContactsComponent
+    },
+    {
+        path:"/auth/create",
+        component:CreateContactComponent
+    }
+]
